2ef918afee993d927d2538d1ec6506a47ce38549
jean2k17 <C16356363@mydit.ie>
2019-10-18 22:54:09 +0100

    Add files via upload

---
+++ Assets/Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public PlayerMovement move;
+    void onCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.GetComponent<Collider>().tag == "obs")
+        {
+            move.enabled = false;
+        }
+    } 
+}
